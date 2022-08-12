5fe1e70db98e3d520b2a2dd89b22c036d170cb7c
CUBRID <kdm97912@gmail.com>
2022-08-12 23:11:20 +0900

    0812 백준

--- 코딩_테스트/백준/python/test.py
+++ 코딩_테스트/백준/python/test.py
@@ -1,20 +1,3 @@
-n = int(input())
-arr1 = list(map(int, input().split()))
+l = [1,2,3,4,5]
 
-dict1 = dict()
-# 숫자카드와 개수를 딕셔너리에 담기
-for i in arr1:
-    if i in dict1:
-        dict1[i] += 1
-    else:
-        dict1[i] = 1
-
-
-m = int(input())
-arr2 = list(map(int, input().split()))
-
-for i in arr2:
-    if i in dict1:
-        print(dict1[i], end=' ')    # 존재하는 숫자 카드라면
-    else:
-        print(0, end=' ')
+print(l[-1])
