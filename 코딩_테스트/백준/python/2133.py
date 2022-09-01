501bb3e7078d8f89470ff4441041f0dc402349a2
CUBRID <kdm97912@gmail.com>
2022-09-01 17:39:20 +0900

    0901 백준

--- 코딩_테스트/백준/python/2133.py
+++ 코딩_테스트/백준/python/2133.py
@@ -21,9 +21,13 @@ import sys
 n = int(sys.stdin.readline())
 d = [0] * (31)
 
+d[0] = 1
+d[1] = 0
 d[2] = 3
-d[4] = 11
-for i in range(5, n+1):
-    d[i] = d[i-2] * 3 + d[i-4] * 8
+
+for i in range(4, n+1, 2):
+    d[i] += 3 * d[i-2]
+    for j in range(4, i+1, 2):
+        d[i] += 2 * d[i-j]
 
 print(d[n])
