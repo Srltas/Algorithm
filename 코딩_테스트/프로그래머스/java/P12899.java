b1254a016b389b2a7de78be84b2d93a2a08ba878
CUBRID <kdm97912@gmail.com>
2022-09-07 09:19:59 +0900

    0907 백준

---
+++ 코딩_테스트/프로그래머스/java/P12899.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class P12899 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Solution solution = new Solution();
+        System.out.println(solution.solution(scanner.nextInt()));
+    }
+
+    static class Solution {
+        public String solution(int n) {
+            String answer = "";
+            String[] num = {"4", "1", "2"};
+
+            while (n > 0) {
+                answer = num[n % 3] + answer;
+                n = (n - 1) / 3;
+            }
+
+            return answer;
+        }
+    }
+}
