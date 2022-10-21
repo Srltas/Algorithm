c637fa718f1cc97a1018797558c1f7db8739c586
srltas <kdm97912@gmail.com>
2022-10-21 23:49:20 +0900

    [BOJ-11050-JAVA] 이항 계수 1

--- Beakjoon/Java/bronze/P11050.java
+++ Beakjoon/Java/bronze/P11050.java
@@ -1,26 +1,64 @@
+/**
+ *
+ * URL : https://www.acmicpc.net/problem/11050
+ *
+ * 이항 계수 1
+ *
+ * 문제
+ * 자연수 \(N\)과 정수 \(K\)가 주어졌을 때 이항 계수
+ * \(\binom{N}{K}\)를 구하는 프로그램을 작성하시오.
+ *
+ * 입력
+ * 첫째 줄에 \(N\)과 \(K\)가 주어진다. (1 ≤ \(N\) ≤ 10, 0 ≤ \(K\) ≤ \(N\))
+ *
+ * 출력
+ *
+ * \(\binom{N}{K}\)를 출력한다.
+ *
+ * 예제 입력 1
+ * 5 2
+ * 예제 출력 1
+ * 10
+ */
+
 package bronze;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class P11050 {
+//    public static void main(String[] args) throws IOException {
+//        Scanner sc = new Scanner(System.in);
+//        int N = sc.nextInt();
+//        int K = sc.nextInt();
+//
+//        int[][] dp = new int[N + 1][N + 1];
+//        for (int i = 0; i <= N; i++) {
+//            dp[i][1] = i;   // i개 중 1개를 선택하는 경우의 수 i
+//            dp[i][0] = 1;   // i개 중 0개를 선택하는 경우의 수 1
+//            dp[i][i] = 1;   // i개 중 i개를 선택하는 경우의 수 1
+//        }
+//
+//        for (int i = 2; i <= N; i++) {
+//            for (int j = 2; j <= i; j++) {
+//                dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1];
+//            }
+//        }
+//
+//        System.out.println(dp[N][K]);
+//    }
 
-    static int[][] dp;
+    private static int[][] dp;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+        dp = new int[N + 1][N + 1];
 
-        dp = new int[N + 1][K + 1];
-
-        System.out.println(bc(N,K));
+        System.out.println(getDP(N, K));
     }
 
-    static int bc(int n, int k) {
+    private static int getDP(int n, int k) {
         if (dp[n][k] > 0) {
             return dp[n][k];
         }
@@ -29,6 +67,6 @@ public class P11050 {
             return dp[n][k] = 1;
         }
 
-        return dp[n][k] = bc(n - 1, k - 1) + bc(n - 1, k);
+        return dp[n][k] = getDP(n - 1, k) + getDP(n - 1, k - 1);
     }
 }
