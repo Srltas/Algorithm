3a305f8a03219e3c221a50db90fd2136336d7c85
Srltas <kdm97912@gmail.com>
2023-04-21 09:49:58 +0900

    [BOJ-1956] 운동

---
+++ Beakjoon/Java/gold/P1956.java
@@ -0,0 +1,102 @@
+/**
+ *
+ * URL : https://www.acmicpc.net/problem/1956
+ *
+ * 운동
+ *
+ * 문제
+ * V개의 마을와 E개의 도로로 구성되어 있는 도시가 있다. 도로는 마을과 마을 사이에 놓여 있으며, 일방 통행 도로이다. 마을에는 편의상 1번부터 V번까지 번호가 매겨져 있다고 하자.
+ *
+ * 당신은 도로를 따라 운동을 하기 위한 경로를 찾으려고 한다. 운동을 한 후에는 다시 시작점으로 돌아오는 것이 좋기 때문에, 우리는 사이클을 찾기를 원한다. 단, 당신은 운동을 매우 귀찮아하므로, 사이클을 이루는 도로의 길이의 합이 최소가 되도록 찾으려고 한다.
+ *
+ * 도로의 정보가 주어졌을 때, 도로의 길이의 합이 가장 작은 사이클을 찾는 프로그램을 작성하시오. 두 마을을 왕복하는 경우도 사이클에 포함됨에 주의한다.
+ *
+ * 입력
+ * 첫째 줄에 V와 E가 빈칸을 사이에 두고 주어진다. (2 ≤ V ≤ 400, 0 ≤ E ≤ V(V-1)) 다음 E개의 줄에는 각각 세 개의 정수 a, b, c가 주어진다. a번 마을에서 b번 마을로 가는 거리가 c인 도로가 있다는 의미이다. (a → b임에 주의) 거리는 10,000 이하의 자연수이다. (a, b) 쌍이 같은 도로가 여러 번 주어지지 않는다.
+ *
+ * 출력
+ * 첫째 줄에 최소 사이클의 도로 길이의 합을 출력한다. 운동 경로를 찾는 것이 불가능한 경우에는 -1을 출력한다.
+ *
+ * 예제 입력 1
+ * 3 4
+ * 1 2 1
+ * 3 2 1
+ * 1 3 5
+ * 2 3 2
+ * 예제 출력 1
+ * 3
+ */
+
+package gold;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class P1956 {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringTokenizer st;
+
+    final int MAX_VALUE = 9999999;
+
+    public static void main(String[] args) throws IOException {
+        new P1956().solution();
+    }
+
+    public void solution() throws IOException {
+        st = new StringTokenizer(br.readLine());
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+
+        int[][] distance = new int[V + 1][V + 1];
+        for (int i = 1; i <= V; i++) {
+            for (int j = 1; j <= V; j++) {
+                if (i != j) {
+                    distance[i][j] = MAX_VALUE;
+                }
+            }
+        }
+
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            distance[a][b] = c;
+        }
+
+        for (int k = 1; k <= V; k++) {
+            for (int i = 1; i <= V; i++) {
+                for (int j = 1; j <= V; j++) {
+                    if (i == j) {
+                        continue;
+                    }
+
+                    if (distance[i][j] > distance[i][k] + distance[k][j]) {
+                        distance[i][j] = distance[i][k] + distance[k][j];
+                    }
+                }
+            }
+        }
+
+        int cycleValue = MAX_VALUE;
+        for (int i = 1; i <= V; i++) {
+            for (int j = 1; j <= V; j++) {
+                if (i == j) {
+                    continue;
+                }
+
+                if (distance[i][j] != MAX_VALUE && distance[j][i] != MAX_VALUE) {
+                    cycleValue = Math.min(cycleValue, distance[i][j] + distance[j][i]);
+                }
+            }
+        }
+        cycleValue = cycleValue == MAX_VALUE ? -1 : cycleValue;
+        bw.write(cycleValue + System.lineSeparator());
+        bw.flush();
+
+        bw.close();
+        br.close();
+    }
+}
