77e4b85124f2593d9adb18832da16269000adc89
Srltas <kdm97912@gmail.com>
2023-04-29 20:30:07 +0900

    [BOJ-27866] 문자와 문자열

---
+++ Beakjoon/Java/bronze/P27866.java
@@ -0,0 +1,69 @@
+/**
+ *
+ * URL : https://www.acmicpc.net/problem/27866
+ *
+ * 문자와 문자열
+ *
+ * 문제
+ * 단어
+ * $S$와 정수
+ * $i$가 주어졌을 때,
+ * $S$의
+ * $i$번째 글자를 출력하는 프로그램을 작성하시오.
+ *
+ * 입력
+ * 첫째 줄에 영어 소문자와 대문자로만 이루어진 단어
+ * $S$가 주어진다. 단어의 길이는 최대
+ * $1\,000$이다.
+ *
+ * 둘째 줄에 정수
+ * $i$가 주어진다. (
+ * $1 \le i \le \left|S\right|$)
+ *
+ * 출력
+ *
+ * $S$의
+ * $i$번째 글자를 출력한다.
+ *
+ * 예제 입력 1
+ * Sprout
+ * 3
+ * 예제 출력 1
+ * r
+ * 예제 입력 2
+ * shiftpsh
+ * 6
+ * 예제 출력 2
+ * p
+ * 예제 입력 3
+ * Baekjoon
+ * 4
+ * 예제 출력 3
+ * k
+ */
+
+package bronze;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class P27866 {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        new P27866().solution();
+    }
+
+    public void solution() throws IOException {
+        String st = br.readLine();
+        int num = Integer.parseInt(br.readLine());
+
+        bw.write(st.charAt(num - 1));
+        bw.flush();
+
+        bw.close();
+        br.close();
+    }
+}
