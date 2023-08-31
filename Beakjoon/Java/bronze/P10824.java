6f03dcca26113fa336d22efabf23822e31b4e3d0
Srltas <kdm97912@gmail.com>
2023-08-31 23:12:32 +0900

    [BOJ-10824] 네 수

---
+++ Beakjoon/Java/bronze/P10824.java
@@ -0,0 +1,55 @@
+/**
+ *
+ * URL : https://www.acmicpc.net/problem/10824
+ *
+ * 네 수
+ *
+ * 문제
+ * 네 자연수 A, B, C, D가 주어진다. 이때, A와 B를 붙인 수와 C와 D를 붙인 수의 합을 구하는 프로그램을 작성하시오.
+ *
+ * 두 수 A와 B를 합치는 것은 A의 뒤에 B를 붙이는 것을 의미한다. 즉, 20과 30을 붙이면 2030이 된다.
+ *
+ * 입력
+ * 첫째 줄에 네 자연수 A, B, C, D가 주어진다. (1 ≤ A, B, C, D ≤ 1,000,000)
+ *
+ * 출력
+ * A와 B를 붙인 수와 C와 D를 붙인 수의 합을 출력한다.
+ *
+ * 예제 입력 1
+ * 10 20 30 40
+ * 예제 출력 1
+ * 4060
+ */
+
+package bronze;
+
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class P10824 {
+  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+  StringTokenizer st;
+
+  public static void main(String[] args) throws IOException {
+      new P10824().solution();
+  }
+
+  public void solution() throws IOException {
+    st = new StringTokenizer(br.readLine());
+    String A = st.nextToken();
+    String B = st.nextToken();
+    String C = st.nextToken();
+    String D = st.nextToken();
+
+    BigInteger AB = new BigInteger((A + B));
+    BigInteger CD = new BigInteger((C + D));
+
+    bw.write((AB.add(CD)) + System.lineSeparator());
+    bw.flush();
+
+    bw.close();
+    br.close();
+  }
+}
