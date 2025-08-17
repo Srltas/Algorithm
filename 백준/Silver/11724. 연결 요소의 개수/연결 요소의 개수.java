44667b7a777d4a8d4432f97591402308a6b03d4e
Dongmin Kim <49967405+Srltas@users.noreply.github.com>
2025-08-18 00:33:02 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 440 ms, Memory: 112348 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,54 +1,59 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    private static ArrayList<Integer>[] A;
-    private static boolean visited[];
-    
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+  static int[] parent;
+  static int[] size;
 
-        A = new ArrayList[N + 1];
-        visited = new boolean[N + 1];
-        for (int i = 1; i < N + 1; i++) {
-            A[i] = new ArrayList<Integer>();
-        }
+  public static void main(String[] args) throws IOException {
+    try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int N = Integer.parseInt(st.nextToken());
+      int M = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int S = Integer.parseInt(st.nextToken());
-            int E = Integer.parseInt(st.nextToken());
+      parent = new int[N + 1];
+      size = new int[N + 1];
+      for (int i = 1; i <= N; i++) {
+        parent[i] = i;
+        size[i] = 1;
+      }
 
-            A[S].add(E);
-            A[E].add(S);
-        }
+      for (int i = 0; i < M; i++) {
+        st = new StringTokenizer(br.readLine());
+        int u = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
+        union(u, v);
+      }
 
-        int count = 0;
-        for (int i = 1; i < N + 1; i++) {
-            if (!visited[i]) {
-                count++;
-                DFS(i);
-            }
-        }
-
-        System.out.println(count);
+      int count = 0;
+      for (int i = 1; i <= N; i++) {
+        if (find(i) == i) count++;
+      }
+      System.out.println(count);
     }
+  }
 
-    private static void DFS(int V) {
-        if (visited[V]) {
-            return;
-        }
-
-        visited[V] = true;
-        for (int a : A[V]) {
-            DFS(a);
-        }
+  static int find(int x) {
+    while (x != parent[x]) {
+      parent[x] = parent[parent[x]];
+      x = parent[x];
     }
+    return x;
+  }
+
+  static void union(int a, int b) {
+    int ra = find(a);
+    int rb = find(b);
+    if (ra == rb) return;
+    if (size[ra] < size[rb]) {
+      int tmp = ra;
+      ra = rb;
+      rb = tmp;
+    }
+    parent[rb] = ra;
+    size[ra] += size[rb];
+  }
 }
