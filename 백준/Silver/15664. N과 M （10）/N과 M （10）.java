e91dead83a9968613912b58b00247da013a0efa4
Dongmin Kim <49967405+Srltas@users.noreply.github.com>
2025-08-28 07:29:28 +0900

    [Silver II] Title: N과 M (10), Time: 104 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  static int N, M;
+  static int[] path;
+  static int[] numbers;
+  static StringBuilder sb = new StringBuilder();
+
+  public static void main(String[] args) throws IOException {
+    try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      N = Integer.parseInt(st.nextToken());
+      M = Integer.parseInt(st.nextToken());
+
+      path = new int[M];
+      numbers = new int[N];
+      st = new StringTokenizer(br.readLine());
+      for (int i = 0; i < N; i++) numbers[i] = Integer.parseInt(st.nextToken());
+      Arrays.sort(numbers);
+      dfs(0, 0);
+      System.out.println(sb);
+    }
+  }
+
+  static void dfs(int depth, int start) {
+    if (depth == M) {
+      for (int p : path) {
+        sb.append(p).append(" ");
+      }
+      sb.append("\n");
+      return;
+    }
+
+    int limited = N - (M - depth) + 1;
+    int prev = Integer.MIN_VALUE;
+    for (int i = start; i < limited; i++) {
+      if (numbers[i] == prev) continue;
+      path[depth] = numbers[i];
+      prev = numbers[i];
+      dfs(depth + 1, i + 1);
+    }
+  }
+}
