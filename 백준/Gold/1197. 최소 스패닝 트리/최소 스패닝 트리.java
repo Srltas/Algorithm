d9da84ff4d60d4442cb6ee9cb3af34c445f17da2
Dongmin Kim <49967405+Srltas@users.noreply.github.com>
2025-09-04 22:40:47 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 488 ms, Memory: 49260 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -0,0 +1,78 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+
+        PriorityQueue<pEdge> q = new PriorityQueue<>();
+        parent = new int[V + 1];
+        for (int i = 1; i <= V; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            q.offer(new pEdge(s, e, v));
+        }
+
+        int useEdge = 0;
+        int result = 0;
+        while (useEdge < V - 1) {
+            pEdge now = q.poll();
+            if (find(now.s) != find(now.e)) {
+                union(now.s, now.e);
+                result += now.v;
+                useEdge++;
+            }
+        }
+        System.out.println(result);
+    }
+
+    private static int find(int n) {
+        if (n == parent[n]) {
+            return n;
+        }
+        return parent[n] = find(parent[n]);
+    }
+
+    private static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+
+        if (a != b) {
+            parent[b] = a;
+        }
+    }
+}
+
+class pEdge implements Comparable<pEdge> {
+
+    int s;
+    int e;
+    int v;
+
+    public pEdge(int s, int e, int v) {
+        this.s = s;
+        this.e = e;
+        this.v = v;
+    }
+
+    @Override
+    public int compareTo(pEdge o) {
+        return this.v - o.v;
+    }
+}
