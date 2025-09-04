892cf3c4006c9c75b45b4c127b4254c3e35758fb
Dongmin Kim <49967405+Srltas@users.noreply.github.com>
2025-09-04 23:36:20 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 376 ms, Memory: 24304 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,78 +1,96 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class Main {
 
-    private static int[] parent;
+    static int[] parent, rank;
+    static Edge[] edges;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int V = Integer.parseInt(st.nextToken());
-        int E = Integer.parseInt(st.nextToken());
+        FastScanner fs = new FastScanner();
+        int V = fs.nextInt();
+        int E = fs.nextInt();
 
-        PriorityQueue<pEdge> q = new PriorityQueue<>();
         parent = new int[V + 1];
-        for (int i = 1; i <= V; i++) {
-            parent[i] = i;
-        }
+        rank = new int[V + 1];
+        edges = new Edge[E];
+
+        for (int i = 1; i <= V; i++) parent[i] = i;
 
         for (int i = 0; i < E; i++) {
-            st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-
-            q.offer(new pEdge(s, e, v));
+            int a = fs.nextInt();
+            int b = fs.nextInt();
+            int c = fs.nextInt();
+            edges[i] = new Edge(a, b, c);
         }
 
-        int useEdge = 0;
-        int result = 0;
-        while (useEdge < V - 1) {
-            pEdge now = q.poll();
-            if (find(now.s) != find(now.e)) {
-                union(now.s, now.e);
-                result += now.v;
-                useEdge++;
+        Arrays.sort(edges);
+
+        long total = 0L;
+        int used = 0;
+
+        for (Edge e : edges) {
+            int ra = find(e.a);
+            int rb = find(e.b);
+            if (ra == rb) continue;
+            union(ra, rb);
+            total += e.c;
+            if (++used == V - 1) break;
+        }
+        System.out.println(total);
+    }
+
+    static void union(int x, int y) {
+        if (x == y) return;
+        if (rank[x] < rank[y]) {
+            parent[x] = y;
+        } else if (rank[x] > rank[y]) {
+            parent[y] = x;
+        } else {
+            parent[y] = x;
+            rank[x]++;
+        }
+        x = find(x);
+        y = find(y);
+        if (x != y) {
+            parent[y] = x;
+        }
+    }
+
+    static int find(int x) {
+        if (parent[x] != x) parent[x] = find(parent[x]);
+        return parent[x];
+    }
+
+    static class Edge implements Comparable<Edge> {
+        int a, b, c;
+        public Edge(int a, int b, int c) { this.a = a; this.b = b; this.c = c; }
+        @Override
+        public int compareTo(Edge o) { return Integer.compare(this.c, o.c); }
+    }
+
+    static class FastScanner {
+        private final byte[] buffer = new byte[1 << 16];
+        private int ptr = 0, len = 0;
+
+        private int read() throws IOException {
+            if (ptr >= len) {
+                len = System.in.read(buffer);
+                ptr = 0;
+                if (len <= 0) return -1;
             }
+            return buffer[ptr++];
         }
-        System.out.println(result);
-    }
 
-    private static int find(int n) {
-        if (n == parent[n]) {
-            return n;
+        int nextInt() throws IOException {
+            int c, sign = 1, val = 0;
+            do { c= read(); } while (c <= ' ');
+            if (c == '-') { sign = -1; c = read(); }
+            while (c > ' ') {
+                val = val * 10 + (c - '0');
+                c = read();
+            }
+            return val * sign;
         }
-        return parent[n] = find(parent[n]);
-    }
-
-    private static void union(int a, int b) {
-        a = find(a);
-        b = find(b);
-
-        if (a != b) {
-            parent[b] = a;
-        }
-    }
-}
-
-class pEdge implements Comparable<pEdge> {
-
-    int s;
-    int e;
-    int v;
-
-    public pEdge(int s, int e, int v) {
-        this.s = s;
-        this.e = e;
-        this.v = v;
-    }
-
-    @Override
-    public int compareTo(pEdge o) {
-        return this.v - o.v;
     }
 }
