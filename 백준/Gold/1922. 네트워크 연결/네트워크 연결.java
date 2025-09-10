344907a99256894b4dd7b9ded2c201ece51a0cf1
Dongmin Kim <49967405+Srltas@users.noreply.github.com>
2025-09-10 14:34:31 +0900

    [Gold IV] Title: 네트워크 연결, Time: 456 ms, Memory: 51624 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -1,76 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-
-    static int N, M;
-    static Edge[] edges;
-    static int[] parent, rank;
-
     public static void main(String[] args) throws IOException {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            N = Integer.parseInt(br.readLine());
-            M = Integer.parseInt(br.readLine());
+            int N = Integer.parseInt(br.readLine());
+            int M = Integer.parseInt(br.readLine());
 
-            parent = new int[N + 1];
-            rank = new int[N + 1];
-            edges = new Edge[M];
-
-            for (int i = 1; i < N + 1; i++) parent[i] = i;
+            List<Edge>[] nodes = new ArrayList[N + 1];
+            for (int i = 1; i < N + 1; i++) nodes[i] = new ArrayList<>();
 
             for (int i = 0; i < M; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
                 int a = Integer.parseInt(st.nextToken());
                 int b = Integer.parseInt(st.nextToken());
                 int c = Integer.parseInt(st.nextToken());
-                edges[i] = new Edge(a, b, c);
+                nodes[a].add(new Edge(b, c));
+                nodes[b].add(new Edge(a, c));
             }
 
-            Arrays.sort(edges);
+            Queue<Edge> q = new PriorityQueue<>();
+            boolean[] visited = new boolean[N + 1];
+            q.addAll(nodes[1]);
+            visited[1] = true;
 
             int total = 0;
             int used = 0;
-            for (Edge edge : edges) {
-                int na = find(edge.a);
-                int nb = find(edge.b);
-                if (na == nb) continue;
-                union(na, nb);
-                total += edge.c;
-                if (++used > N - 1) break;
+            while (!q.isEmpty() && used < N - 1) {
+                Edge e = q.poll();
+                if (visited[e.b]) continue;
+                visited[e.b] = true;
+                total += e.c;
+                used++;
+                for (Edge ne : nodes[e.b]) {
+                    if (!visited[ne.b]) q.offer(ne);
+                }
             }
             System.out.println(total);
         }
     }
 
-    static void union(int x, int y) {
-        if (x == y) return;
-        if (rank[x] > rank[y]) {
-            parent[y] = x;
-        } else if (rank[y] > rank[x]) {
-            parent[x] = y;
-        } else {
-            parent[y] = x;
-            rank[x]++;
-        }
-    }
-
-    static int find(int x) {
-        if (parent[x] != x) parent[x] = find(parent[x]);
-        return parent[x];
-    }
-
     static class Edge implements Comparable<Edge> {
-        int a, b, c;
-
-        public Edge(int a, int b, int c) {
-            this.a = a;
-            this.b = b;
-            this.c = c;
-        }
-
+        int b, c;
+        public Edge(int b, int c) { this.b = b; this.c = c; }
         @Override
         public int compareTo(Edge o) {
             return this.c - o.c;
