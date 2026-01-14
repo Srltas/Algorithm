b1ba3bcc386c1e2a9da3ebf16bd245ecc0725e19
Dongmin Kim <49967405+Srltas@users.noreply.github.com>
2026-01-14 19:34:53 +0900

    [Silver IV] Title: 덱, Time: 164 ms, Memory: 18784 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.java
+++ 백준/Silver/10866. 덱/덱.java
@@ -3,62 +3,61 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.Optional;
 import java.util.StringTokenizer;
 
 public class Main {
 
+    private static final char NEW_LINE = '\n';
     private static Deque<Integer> deque = new ArrayDeque<>();
+    private static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-
-        StringBuilder sb = new StringBuilder();
+        
         while (N-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             String command = st.nextToken();
-            int num = 0;
-            if (st.hasMoreTokens()) {
-                num = Integer.parseInt(st.nextToken());
+
+            if (command.startsWith("push")) {
+                int num = Integer.parseInt(st.nextToken());
+                executePush(command, num);
+            } else {
+                executePrint(command);
             }
 
-            int result = dequeProgram(command, num);
-            if (result == -2) continue;
-            sb.append(result).append("\n");
         }
         System.out.println(sb);
     }
 
-    private static int dequeProgram(String command, int num) {
-        int result = -2;
+    private static void executePush(String command, int num) {
+        if (command.equals("push_front")) {
+            deque.offerFirst(num);
+        } else {
+            deque.offerLast(num);
+        }
+    }
 
+    private static void executePrint(String command) {
         switch (command) {
-            case "push_back":
-                deque.addLast(num);
-                break;
-            case "push_front":
-                deque.addFirst(num);
-                break;
             case "pop_front":
-                result = Optional.ofNullable(deque.pollFirst()).orElse(-1);
+                sb.append(deque.isEmpty() ? -1 : deque.pollFirst()).append(NEW_LINE);
                 break;
             case "pop_back":
-                result = Optional.ofNullable(deque.pollLast()).orElse(-1);
+                sb.append(deque.isEmpty() ? -1 : deque.pollLast()).append(NEW_LINE);
                 break;
             case "size":
-                result = deque.size();
+                sb.append(deque.size()).append(NEW_LINE);
                 break;
             case "empty":
-                result = deque.isEmpty() ? 1 : 0;
+                sb.append(deque.isEmpty() ? 1 : 0).append(NEW_LINE);
                 break;
             case "front":
-                result = Optional.ofNullable(deque.peekFirst()).orElse(-1);
+                sb.append(deque.isEmpty() ? -1 : deque.peekFirst()).append(NEW_LINE);
                 break;
             case "back":
-                result = Optional.ofNullable(deque.peekLast()).orElse(-1);
+                sb.append(deque.isEmpty() ? -1 : deque.peekLast()).append(NEW_LINE);
+                break;
         }
-
-        return result;
     }
 }
