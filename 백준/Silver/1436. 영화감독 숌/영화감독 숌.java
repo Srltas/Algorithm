4506c73c534669abfc7c710813eeb4b9f160630c
Dongmin Kim <49967405+Srltas@users.noreply.github.com>
2026-01-22 07:28:19 +0900

    [Silver V] Title: 영화감독 숌, Time: 336 ms, Memory: 92616 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int num666 = 666;
+        int count = 1;
+        while (N > count) {
+            num666++;
+            if (String.valueOf(num666).contains("666")) {
+                count++;
+            }
+        }
+        System.out.println(num666);
+    }
+}
