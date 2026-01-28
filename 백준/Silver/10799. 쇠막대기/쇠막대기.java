a1022878ec5aeb0de48abb33d87a5dae600617c8
Dongmin Kim <49967405+Srltas@users.noreply.github.com>
2026-01-29 07:46:17 +0900

    [Silver II] Title: 쇠막대기, Time: 136 ms, Memory: 15656 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -1,43 +1,26 @@
-import java.io.*;
-import java.util.Stack;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        new Main().solution();
-    }
-
-    public void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        String stick = br.readLine();
-
+        String str = br.readLine();
+        int count = 0;
         int result = 0;
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < stick.length(); i++) {
-            char s = stick.charAt(i);
-            if (s == '(') {
-                stack.push('(');
-                continue;
-            }
-
-            if (s == ')') {
-                stack.pop();
-
-                // stack에 남아 있는 ( 개수가 레이저로 잘랐을 때 남는 조각
-                if (stick.charAt(i - 1) == '(') {
-                    result += stack.size();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c == '(') {
+                count++;
+            } else {
+                count--;
+                if (str.charAt(i - 1) == '(') {
+                    result += count;
                 } else {
-                    // 막대기에 끝으로 조각 한개를 더해준다.
-                    result++;
+                    result += 1;
                 }
             }
         }
-
-        bw.write(result + "\n");
-        bw.flush();
-
-        bw.close();
-        br.close();
+        System.out.println(result);
     }
 }
