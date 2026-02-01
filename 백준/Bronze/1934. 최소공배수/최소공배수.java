b00caacd7a359625f23c8c398fd7920eb240e3c4
Dongmin Kim <49967405+Srltas@users.noreply.github.com>
2026-02-02 07:56:37 +0900

    [Bronze I] Title: 최소공배수, Time: 120 ms, Memory: 14612 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            sb.append(LCM(A, B)).append('\n');
+        }
+        System.out.println(sb);
+    }
+
+    static int LCM(int a, int b) {
+        return (a / GCD(a, b)) * b;
+    }
+
+    static int GCD(int a, int b) {
+        while (b != 0) {
+            int r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
+}
