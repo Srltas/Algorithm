8efaa72978315b48edb876ab5baa68b56ccd851d
Dongmin Kim <49967405+Srltas@users.noreply.github.com>
2026-02-03 15:28:37 +0900

    [Silver III] Title: 소수 구하기, Time: 264 ms, Memory: 21736 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        boolean[] isPrime = new boolean[N + 1];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = isPrime[1] = false;
+
+        for (int i = 2; i * i <= N; i++) {
+            if (!isPrime[i]) continue;
+            for (int j = i * i; j <= N; j += i) isPrime[j] = false;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = M; i <= N; i++) {
+            if (isPrime[i]) sb.append(i).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
