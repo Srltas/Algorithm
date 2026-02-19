77d315f73fcc3f688aa4d721a5b109b26cec1055
Dongmin Kim <49967405+Srltas@users.noreply.github.com>
2026-02-19 16:04:09 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 108 ms, Memory: 14084 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -1,53 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.BitSet;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            int N = Integer.parseInt(br.readLine());
-            int[] switches = new int[N + 1];
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int i = 1; i <= N; i++) {
-                switches[i] = Integer.parseInt(st.nextToken());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        BitSet switches = new BitSet(N + 1);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            if (st.nextToken().equals("1")) {
+                switches.set(i);
             }
+        }
 
-            int student = Integer.parseInt(br.readLine());
-            for (int i = 0; i < student; i++) {
-                st = new StringTokenizer(br.readLine());
-                int gender = Integer.parseInt(st.nextToken());
-                int num = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(br.readLine());
+        while (M-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int g = Integer.parseInt(st.nextToken());
+            int n = Integer.parseInt(st.nextToken());
 
-                if (gender == 1) {
-                    for (int j = num; j <= N; j+= num) {
-                        switches[j] = switches[j] == 0 ? 1 : 0;
-                    }
-                } else {
-                    switches[num] = switches[num] == 0 ? 1 : 0;
-                    int left = num - 1;
-                    int right = num + 1;
-                    while (left >= 1 && right <= N) {
-                        if (switches[left] == switches[right]) {
-                            switches[left] = switches[left] == 0 ? 1 : 0;
-                            switches[right] = switches[right] == 0 ? 1 : 0;
-                            left--;
-                            right++;
-                        } else {
-                            break;
-                        }
-                    }
+            if (g == 1) {
+                for (int i = n; i <= N; i += n) {
+                    switches.flip(i);
                 }
-            }
-
-            for (int i = 1; i <= N; i++) {
-                System.out.print(switches[i]);
-                if (i % 20 == 0) {
-                    System.out.println();
-                } else if (i < N) {
-                    System.out.print(" ");
+            } else {
+                switches.flip(n);
+                int l = n - 1;
+                int r = n + 1;
+                while (l >= 1 && r <= N && switches.get(l) == switches.get(r)) {
+                    switches.flip(l--);
+                    switches.flip(r++);
                 }
             }
         }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            sb.append(switches.get(i) ? 1 : 0).append(' ');
+            if (i % 20 == 0) sb.append('\n');
+        }
+        System.out.println(sb);
     }
 }
