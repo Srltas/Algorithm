5ee169f0d368eca9f9fc107f906be7a3af79f509
Dongmin Kim <49967405+Srltas@users.noreply.github.com>
2026-02-20 22:56:27 +0900

    [Gold V] Title: 문자열 게임 2, Time: 380 ms, Memory: 33952 KB -BaekjoonHub

--- 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
+++ 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
@@ -5,51 +5,50 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-  public static void main(String[] args) throws IOException {
-    try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-      int T = Integer.parseInt(br.readLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        while (T-- > 0) {
+            String str = br.readLine();
+            int K = Integer.parseInt(br.readLine());
 
-      while (T-- > 0) {
-        String str = br.readLine();
-        int K = Integer.parseInt(br.readLine());
+            int min = Integer.MAX_VALUE;
+            int max = 0;
 
-        int minLen = Integer.MAX_VALUE;
-        int maxLen = -1;
-
-        List<Integer>[] charIndexes = new ArrayList[26];
-        for (int i = 0; i < 26; i++) {
-          charIndexes[i] = new ArrayList<>();
-        }
-
-        for (int i = 0; i < str.length(); i++) {
-          char c = str.charAt(i);
-          charIndexes[c - 'a'].add(i);
-        }
-
-        for (int i = 0; i < 26; i++) {
-          List<Integer> list = charIndexes[i];
-          if (list.size() < K) {
-            continue;
-          }
-
-          for (int j = 0; j <= list.size() - K; j++) {
-            int start = list.get(j);
-            int end = list.get(j + K - 1);
-            int length = end - start + 1;
-
-            minLen = Math.min(minLen, length);
-            if (str.charAt(start) == str.charAt(end)) {
-              maxLen = Math.max(maxLen, length);
+            List<Integer>[] charIndexes = new ArrayList[26];
+            for (int i = 0; i < 26; i++) {
+                charIndexes[i] = new ArrayList<>();
             }
-          }
-        }
 
-        if (minLen == Integer.MAX_VALUE || maxLen == -1) {
-          System.out.println("-1");
-        } else {
-          System.out.println(minLen + " " + maxLen);
+            for (int i = 0; i < str.length(); i++) {
+                char c = str.charAt(i);
+                charIndexes[c - 'a'].add(i);
+            }
+
+            for (int i = 0; i < 26; i++) {
+                List<Integer> list = charIndexes[i];
+                if (list.size() < K) continue;
+
+                for (int j = 0; j <= list.size() - K; j++) {
+                    int start = list.get(j);
+                    int end = list.get(j + K - 1);
+                    int length = end - start + 1;
+
+                    min = Math.min(min, length);
+                    if (str.charAt(start) == str.charAt(end)) {
+                        max = Math.max(max, length);
+                    }
+                }
+            }
+
+            if (min == Integer.MAX_VALUE || max == 0) {
+                sb.append("-1");
+            } else {
+                sb.append(min + " " + max);
+            }
+            sb.append('\n');
         }
-      }
+        System.out.println(sb);
     }
-  }
 }
